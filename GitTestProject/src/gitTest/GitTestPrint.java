63f2513ace352f5ba442acdec1705c28328d77d3
banant <banant@banant-VirtualBox>
2012-11-17 22:52:46 -0800

    commit three

--- GitTestProject/src/gitTest/GitTestPrint.java
+++ GitTestProject/src/gitTest/GitTestPrint.java
@@ -6,7 +6,7 @@ public class GitTestPrint {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Testing git");
+		System.out.println("Testing git again");
 
 	}
 
